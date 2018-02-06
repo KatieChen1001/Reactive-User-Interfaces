dd0aa2934ed50ce99c52b49f4604939acf25066b
K80CHEN <mzc219@nyu.edu>
2018-02-06 15:16:02 +0800

    optimized code

--- week2hw/src/App.js
+++ week2hw/src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-
 import Article from './Article.js';
-import Date from './Date.js';
 // No need to import component css here in the main app //
 import './App.css';
 
@@ -13,46 +11,25 @@ import './App.css';
 
 
 class App extends Component {
-  // const = newFeed {
-  //   title: "this is a title",
-  //   description: "this is a description",
-  //   author: "I am the author"
-  // }
 
   render() {
 
+    const newsFeed1 = {
+      time: "Feb. 01, 2018",
+      title: "Facebook Aims to Soothe Wall Street Over News Feed Changes.",
+      description: "The social network, which is overhauling what people consume on the site, said its business would remain robust even if some users spent less time on its platform.",
+      author: "Sheera Frenkel",
+      imgURL: "https://static01.nyt.com/images/2018/02/01/business/01FACEBOOKprint/merlin_131869010_a67bab2f-5fce-4ecc-b528-3a7e84c4cf68-mediumThreeByTwo210.jpg"
+    }
+
+
+
 
     return (
-      <div className = "container">
-        <div className = "newsFeed">
-          <Date date = "Feb. 02, 2018" />
-          <Article
-          title="Facebook Aims to Soothe Wall Street Over News Feed Changes"
-          description = "For a generation, Britain has eagerly enlisted private contractors to provide social services. But the model may be cracking under the pressure of austerity."
-          author = "By SHEERA FRENKEL"
-          />
-          <img className = "myImg" src = "https://static01.nyt.com/images/2018/02/01/business/01FACEBOOKprint/merlin_131869010_a67bab2f-5fce-4ecc-b528-3a7e84c4cf68-mediumThreeByTwo210.jpg" />
-        </div>
-
-        <div className = "newsFeed">
-          <Date date = "Feb. 02, 2018" />
-          <Article
-          title="What’s the Craziest Thing You’ve Ever Found in a Xerox Machine?"
-          description = "The bulky copiers could soon become an office relic. Tell us your stories of documents that made you laugh, blush or learn something new."
-          author = "By LARA TAKENAGA"
-          />
-          <img className = "myImg" src = "https://static01.nyt.com/images/2018/02/01/world/01Xerox/01Xerox-mediumThreeByTwo210-v2.jpg" />
-        </div>
-
-        <div className = "newsFeed">
-          <Date date = "Feb. 02, 2018" />
-          <Article
-          title="Trump’s Environmental Rollbacks Were Fast. It Could Get Messy in Court."
-          description = "Legal experts say many were made without fully considering the laws and procedures governing changes, making them vulnerable to legal challenges."
-          author = "By CORAL DAVENPORT"
-          />
-          <img className = "myImg" src = "https://static01.nyt.com/images/2018/02/01/climate/01CLI-INTERIOR2/01CLI-INTERIOR2-mediumThreeByTwo210.jpg" />
-        </div>
+      <div>
+          <Article time={newsFeed1.time} title={newsFeed1.title} description={newsFeed1.description} author={newsFeed1.author} imgURL={newsFeed1.imgURL}/>
+          <Article time={newsFeed1.time} title={newsFeed1.title} description={newsFeed1.description} author={newsFeed1.author} imgURL={newsFeed1.imgURL}/>
+          <Article time={newsFeed1.time} title={newsFeed1.title} description={newsFeed1.description} author={newsFeed1.author} imgURL={newsFeed1.imgURL}/>
 
       </div>
 
--- week2hw/src/Article.js
+++ week2hw/src/Article.js
@@ -4,10 +4,14 @@ import './Article.css';
 class Article extends Component{
   render (){
     return (
-        <div className = "articleContainer">
-          <h2 className = "articleTitle">{this.props.title}</h2>
-          <p className = "articleDescription">{this.props.description}</p>
-          <p className = "articleAuthor">{this.props.author}</p>
+        <div className="container">
+          <time className="todayDate">{this.props.time}</time>
+          <div className = "articleContainer">
+            <h2 className = "articleTitle">{this.props.title}</h2>
+            <p className = "articleDescription">{this.props.description}</p>
+            <p className = "articleAuthor">By <span>{this.props.author}</span></p>
+          </div>
+          <figure><img src = {this.props.imgURL} /></figure>
         </div>
     )
 
